2b1e0a46ed322faefe1959865eeff6d96678834f
ZiboZhao <zibo.zhao@northwestern.edu>
2021-07-08 22:47:33 -0500

    update script for PyBank analysis

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,31 +9,19 @@ with open(csvpath) as csvfile:
     for row in csvreader:
         date.append(row[0])
         amount.append(int(row[1]))
-#uncomment below to check whether the items returned are correct
-#print(date)
-#print(amount)
 
 #1. The total number of months included in the dataset
 totalMonth = len(date)
-#print(totalMonth)
 
 #2. The net total amount of "Profit/Losses" over the entire period
-totalAmount = 0
-for i in range(len(amount)):
-    totalAmount = totalAmount + amount[i]
-#print(totalAmount)
+totalAmount = sum(amount)
 
 #3. Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
 change = []
 for i in range(len(amount)-1):
     change.append(amount[i+1]-amount[i])
-totalChange = 0
-for i in range(len(change)):
-    totalChange = totalChange + change[i]
-averageChange = totalChange / ((totalMonth) - 1)
-averageChange = round(averageChange,2)
-#print(averageChange)
-
+totalChange = sum(change)
+averageChange = round(totalChange / (totalMonth - 1),2)
 
 #4. The greatest increase in profits (date and amount) over the entire period
 profit_Inc = change[0]
@@ -41,8 +29,6 @@ for i in range(len(change)-1):
     if change[i+1] > profit_Inc:
         date_Inc = date[i+2]
         profit_Inc = change[i+1]
-#print(date_Inc)
-#print(profit_Inc)
 
 #5. The greatest decrease in profits (date and amount) over the entire period
 profit_Dec = change[0]
@@ -50,8 +36,6 @@ for i in range(len(change)-1):
     if change[i+1] < profit_Dec:
         date_Dec = date[i+2]
         profit_Dec = change[i+1]
-#print(date_Dec)
-#print(profit_Dec)
 
 #print out the results
 str1 = "Financial Analysis"
